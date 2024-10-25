cc63dfc7e4a2ef67f6a7d9c13f9a830a414e2a38
RomainGTech <rboisseau@gaming.tech>
2024-10-25 09:01:12 +0200

    y

--- PvZ/AttackAction.cpp
+++ PvZ/AttackAction.cpp
@@ -10,7 +10,8 @@ void AttackAction::Update(Entity* entity)
 {
 	for (int i = 0; i < Playground::getInstance()->GetPlant().size(); i++)
 	{
-		if (Playground::getInstance()->GetPlant()[i]->getPosition().y == entity->getPosition().y && Playground::getInstance()->GetPlant()[i]->getPosition().x + 50 >= entity->getPosition().x) 
+		if (Playground::getInstance()->GetPlant()[i]->getPosition().y == entity->getPosition().y
+			&& Playground::getInstance()->GetPlant()[i]->getPosition().x + 50 >= entity->getPosition().x) 
 		{
 			Zombies* zombie = (Zombies*)entity;
 			Plant* plant = (Plant*)Playground::getInstance()->GetPlant()[i];
--- PvZ/AttackCondition.cpp
+++ PvZ/AttackCondition.cpp
@@ -6,7 +6,8 @@ bool AttackCondition::Test(Entity* entity)
 {
 	for (int i = 0; i < Playground::getInstance()->GetPlant().size(); i++)
 	{
-		if (Playground::getInstance()->GetPlant()[i]->getPosition().y == entity->getPosition().y && Playground::getInstance()->GetPlant()[i]->getPosition().x + 50 >= entity->getPosition().x)
+		if (Playground::getInstance()->GetPlant()[i]->getPosition().y == entity->getPosition().y 
+			&& Playground::getInstance()->GetPlant()[i]->getPosition().x + 50 >= entity->getPosition().x)
 			return true;
 	}
 	return false;
